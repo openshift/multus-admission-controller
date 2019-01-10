fdcd611bf8134c338386d010bbee19718eed87ef
dougbtv <dosmith@redhat.com>
2019-01-10 08:47:29 -0500

    [feature] Improves annotation parsing for namespace isolation

--- pkg/webhook/webhook.go
+++ pkg/webhook/webhook.go
@@ -45,6 +45,7 @@ type jsonPatchOperation struct {
 const (
 	networksAnnotationKey  = "k8s.v1.cni.cncf.io/networks"
 	networkResourceNameKey = "k8s.v1.cni.cncf.io/resourceName"
+	namespaceConstraint    = "_local"
 )
 
 var (
@@ -176,17 +177,112 @@ func analyzeIsolationAnnotation(ar *v1beta1.AdmissionReview) (bool, error) {
 	}
 
 	if len(annotations[networksAnnotationKey]) > 0 {
+
 		glog.Infof("Analyzing %s annotation: %s", networksAnnotationKey, annotations[networksAnnotationKey])
-		if strings.Contains(annotations[networksAnnotationKey], "/") {
-			annotationerrostring := fmt.Sprintf("%s annotations must not refer to namespaced values (must use local namespace, i.e. must not contain a /), rejected: %s", networksAnnotationKey, annotations[networksAnnotationKey])
-			annotationerror := errors.New(annotationerrostring)
-			return false, annotationerror
+
+		networks, err := parsePodNetworkAnnotation(annotations[networksAnnotationKey], namespaceConstraint)
+		if err != nil {
+			glog.Errorf("Error during parsePodNetworkAnnotation: %v", err)
+			return false, err
 		}
+
+		for _, item := range networks {
+			fmt.Printf("name: %v", item.Namespace)
+			if item.Namespace != namespaceConstraint {
+				annotationerrorstring := fmt.Sprintf("%s annotations must not refer to namespaced values (must use local namespace, i.e. must not contain a /), rejected: %s (namespace: %s)", networksAnnotationKey, annotations[networksAnnotationKey], item.Namespace)
+				annotationerror := errors.New(annotationerrorstring)
+				return false, annotationerror
+			}
+		}
+
 		glog.Infof("Allowed value: %s", annotations[networksAnnotationKey])
+
 	}
 
 	return true, nil
 
+}
+
+func parsePodNetworkAnnotation(podNetworks, defaultNamespace string) ([]*types.NetworkSelectionElement, error) {
+	var networks []*types.NetworkSelectionElement
+
+	// logging.Debugf("parsePodNetworkAnnotation: %s, %s", podNetworks, defaultNamespace)
+	if podNetworks == "" {
+		return nil, fmt.Errorf("parsePodNetworkAnnotation: pod annotation not having \"network\" as key, refer Multus README.md for the usage guide")
+	}
+
+	if strings.IndexAny(podNetworks, "[{\"") >= 0 {
+		if err := json.Unmarshal([]byte(podNetworks), &networks); err != nil {
+			return nil, fmt.Errorf("parsePodNetworkAnnotation: failed to parse pod Network Attachment Selection Annotation JSON format: %v", err)
+		}
+	} else {
+		// Comma-delimited list of network attachment object names
+		for _, item := range strings.Split(podNetworks, ",") {
+			// Remove leading and trailing whitespace.
+			item = strings.TrimSpace(item)
+
+			// Parse network name (i.e. <namespace>/<network name>@<ifname>)
+			netNsName, networkName, netIfName, err := parsePodNetworkObjectName(item)
+			if err != nil {
+				return nil, fmt.Errorf("parsePodNetworkAnnotation: %v", err)
+			}
+
+			networks = append(networks, &types.NetworkSelectionElement{
+				Name:             networkName,
+				Namespace:        netNsName,
+				InterfaceRequest: netIfName,
+			})
+		}
+	}
+
+	for _, net := range networks {
+		if net.Namespace == "" {
+			net.Namespace = defaultNamespace
+		}
+	}
+
+	return networks, nil
+}
+
+func parsePodNetworkObjectName(podnetwork string) (string, string, string, error) {
+	var netNsName string
+	var netIfName string
+	var networkName string
+
+	// logging.Debugf("parsePodNetworkObjectName: %s", podnetwork)
+	slashItems := strings.Split(podnetwork, "/")
+	if len(slashItems) == 2 {
+		netNsName = strings.TrimSpace(slashItems[0])
+		networkName = slashItems[1]
+	} else if len(slashItems) == 1 {
+		networkName = slashItems[0]
+	} else {
+		return "", "", "", fmt.Errorf("Invalid network object (failed at '/')")
+	}
+
+	atItems := strings.Split(networkName, "@")
+	networkName = strings.TrimSpace(atItems[0])
+	if len(atItems) == 2 {
+		netIfName = strings.TrimSpace(atItems[1])
+	} else if len(atItems) != 1 {
+		return "", "", "", fmt.Errorf("Invalid network object (failed at '@')")
+	}
+
+	// Check and see if each item matches the specification for valid attachment name.
+	// "Valid attachment names must be comprised of units of the DNS-1123 label format"
+	// [a-z0-9]([-a-z0-9]*[a-z0-9])?
+	// And we allow at (@), and forward slash (/) (units separated by commas)
+	// It must start and end alphanumerically.
+	allItems := []string{netNsName, networkName, netIfName}
+	for i := range allItems {
+		matched, _ := regexp.MatchString("^[a-z0-9]([-a-z0-9]*[a-z0-9])?$", allItems[i])
+		if !matched && len([]rune(allItems[i])) > 0 {
+			return "", "", "", fmt.Errorf(fmt.Sprintf("Failed to parse: one or more items did not match comma-delimited format (must consist of lower case alphanumeric characters). Must start and end with an alphanumeric character), mismatch @ '%v'", allItems[i]))
+		}
+	}
+
+	// logging.Debugf("parsePodNetworkObjectName: parsed: %s, %s, %s", netNsName, networkName, netIfName)
+	return netNsName, networkName, netIfName, nil
 }
 
 func deserializeNetworkAttachmentDefinition(ar *v1beta1.AdmissionReview) (types.NetworkAttachmentDefinition, error) {
