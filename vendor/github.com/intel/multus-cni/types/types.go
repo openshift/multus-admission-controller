699cd32bf06855c49fbb2442cbc8f920a7804c14
Tomofumi Hayashi <tohayash@redhat.com>
2020-03-24 21:58:27 +0900

    Update vendor packages

--- vendor/github.com/intel/multus-cni/types/types.go
+++ vendor/github.com/intel/multus-cni/types/types.go
@@ -20,7 +20,7 @@ import (
 
 	"github.com/containernetworking/cni/pkg/types"
 	"github.com/containernetworking/cni/pkg/types/current"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	v1 "k8s.io/api/core/v1"
 )
 
 // NetConf for cni config file written in json
@@ -36,27 +36,72 @@ type NetConf struct {
 	CNIDir  string `json:"cniDir"`
 	BinDir  string `json:"binDir"`
 	// RawDelegates is private to the NetConf class; use Delegates instead
-	RawDelegates []map[string]interface{} `json:"delegates"`
-	Delegates    []*DelegateNetConf       `json:"-"`
-	NetStatus    []*NetworkStatus         `json:"-"`
-	Kubeconfig   string                   `json:"kubeconfig"`
-	LogFile      string                   `json:"logFile"`
-	LogLevel     string                   `json:"logLevel"`
+	RawDelegates    []map[string]interface{} `json:"delegates"`
+	Delegates       []*DelegateNetConf       `json:"-"`
+	NetStatus       []*NetworkStatus         `json:"-"`
+	Kubeconfig      string                   `json:"kubeconfig"`
+	ClusterNetwork  string                   `json:"clusterNetwork"`
+	DefaultNetworks []string                 `json:"defaultNetworks"`
+	LogFile         string                   `json:"logFile"`
+	LogLevel        string                   `json:"logLevel"`
+	RuntimeConfig   *RuntimeConfig           `json:"runtimeConfig,omitempty"`
+	// Default network readiness options
+	ReadinessIndicatorFile string `json:"readinessindicatorfile"`
+	// Option to isolate the usage of CR's to the namespace in which a pod resides.
+	NamespaceIsolation bool `json:"namespaceIsolation"`
+	// Option to set system namespaces (to avoid to add defaultNetworks)
+	SystemNamespaces []string `json:"systemNamespaces"`
+	// Option to set the namespace that multus-cni uses (clusterNetwork/defaultNetworks)
+	MultusNamespace string `json:"multusNamespace"`
 }
 
+// RuntimeConfig specifies CNI RuntimeConfig
+type RuntimeConfig struct {
+	PortMaps  []*PortMapEntry `json:"portMappings,omitempty"`
+	Bandwidth *BandwidthEntry `json:"bandwidth,omitempty"`
+	IPs       []string        `json:"ips,omitempty"`
+	Mac       string          `json:"mac,omitempty"`
+}
+
+// PortMapEntry for CNI PortMapEntry
+type PortMapEntry struct {
+	HostPort      int    `json:"hostPort"`
+	ContainerPort int    `json:"containerPort"`
+	Protocol      string `json:"protocol,omitempty"`
+	HostIP        string `json:"hostIP,omitempty"`
+}
+
+// BandwidthEntry for CNI BandwidthEntry
+type BandwidthEntry struct {
+	IngressRate  int `json:"ingressRate"`
+	IngressBurst int `json:"ingressBurst"`
+
+	EgressRate  int `json:"egressRate"`
+	EgressBurst int `json:"egressBurst"`
+}
+
+// NetworkStatus is for network status annotation for pod
 type NetworkStatus struct {
 	Name      string    `json:"name"`
 	Interface string    `json:"interface,omitempty"`
 	IPs       []string  `json:"ips,omitempty"`
 	Mac       string    `json:"mac,omitempty"`
-	Default   bool      `json:"default,omitempty"`
 	DNS       types.DNS `json:"dns,omitempty"`
+	Gateway   []net.IP  `json:"default-route,omitempty"`
 }
 
+// DelegateNetConf for net-attach-def for pod
 type DelegateNetConf struct {
-	Conf          types.NetConf
-	ConfList      types.NetConfList
-	IfnameRequest string `json:"ifnameRequest,omitempty"`
+	Conf                types.NetConf
+	ConfList            types.NetConfList
+	Name                string
+	IfnameRequest       string          `json:"ifnameRequest,omitempty"`
+	MacRequest          string          `json:"macRequest,omitempty"`
+	IPRequest           []string        `json:"ipRequest,omitempty"`
+	PortMappingsRequest []*PortMapEntry `json:"-"`
+	BandwidthRequest    *BandwidthEntry `json:"-"`
+	GatewayRequest      []net.IP        `json:"default-route,omitempty"`
+	IsFilterGateway     bool
 	// MasterPlugin is only used internal housekeeping
 	MasterPlugin bool `json:"-"`
 	// Conflist plugin is only used internal housekeeping
@@ -64,31 +109,6 @@ type DelegateNetConf struct {
 
 	// Raw JSON
 	Bytes []byte
-}
-
-type NetworkAttachmentDefinition struct {
-	metav1.TypeMeta `json:",inline"`
-	// Note that ObjectMeta is mandatory, as an object
-	// name is required
-	Metadata metav1.ObjectMeta `json:"metadata,omitempty" description:"standard object metadata"`
-
-	// Specification describing how to invoke a CNI plugin to
-	// add or remove network attachments for a Pod.
-	// In the absence of valid keys in a Spec, the runtime (or
-	// meta-plugin) should load and execute a CNI .configlist
-	// or .config (in that order) file on-disk whose JSON
-	// “name” key matches this Network object’s name.
-	// +optional
-	Spec NetworkAttachmentDefinitionSpec `json:"spec"`
-}
-
-type NetworkAttachmentDefinitionSpec struct {
-	// Config contains a standard JSON-encoded CNI configuration
-	// or configuration list which defines the plugin chain to
-	// execute.  If present, this key takes precedence over
-	// ‘Plugin’.
-	// +optional
-	Config string `json:"config"`
 }
 
 // NetworkSelectionElement represents one element of the JSON format
@@ -102,13 +122,26 @@ type NetworkSelectionElement struct {
 	Namespace string `json:"namespace,omitempty"`
 	// IPRequest contains an optional requested IP address for this network
 	// attachment
-	IPRequest string `json:"ipRequest,omitempty"`
+	IPRequest []string `json:"ips,omitempty"`
 	// MacRequest contains an optional requested MAC address for this
 	// network attachment
-	MacRequest string `json:"macRequest,omitempty"`
+	MacRequest string `json:"mac,omitempty"`
 	// InterfaceRequest contains an optional requested name for the
 	// network interface this attachment will create in the container
-	InterfaceRequest string `json:"interfaceRequest,omitempty"`
+	InterfaceRequest string `json:"interface,omitempty"`
+	// DeprecatedInterfaceRequest is obsolated parameter at pre 3.2.
+	// This will be removed in 4.0 release.
+	DeprecatedInterfaceRequest string `json:"interfaceRequest,omitempty"`
+	// PortMappingsRequest contains an optional requested port mapping
+	// for the network
+	PortMappingsRequest []*PortMapEntry `json:"portMappings,omitempty"`
+	// BandwidthRequest contains an optional requested bandwidth for
+	// the network
+	BandwidthRequest *BandwidthEntry `json:"bandwidth,omitempty"`
+	// CNIArgs contains additional CNI arguments for the network interface
+	CNIArgs *map[string]interface{} `json:"cni-args"`
+	// GatewayRequest contains default route IP address for the pod
+	GatewayRequest []net.IP `json:"default-route,omitempty"`
 }
 
 // K8sArgs is the valid CNI_ARGS used for Kubernetes
@@ -118,4 +151,16 @@ type K8sArgs struct {
 	K8S_POD_NAME               types.UnmarshallableString
 	K8S_POD_NAMESPACE          types.UnmarshallableString
 	K8S_POD_INFRA_CONTAINER_ID types.UnmarshallableString
+}
+
+// ResourceInfo is struct to hold Pod device allocation information
+type ResourceInfo struct {
+	Index     int
+	DeviceIDs []string
+}
+
+// ResourceClient provides a kubelet Pod resource handle
+type ResourceClient interface {
+	// GetPodResourceMap returns an instance of a map of Pod ResourceInfo given a (Pod name, namespace) tuple
+	GetPodResourceMap(*v1.Pod) (map[string]*ResourceInfo, error)
 }
