5417698ca9b770f46f500ab4da3da0572d76b420
Tom Pantelis <tompantelis@gmail.com>
2026-06-08 08:05:52 -0400

    Add TLS configuration CLI flags
    Added optional CLI flags --tls-min-version and --tls-cipher-suites
    for configuring TLS parameters for the webhook and metric servers.
    
    Added unit tests to verify the servers apply the TLS configuration.
    
    main.go was refactored to facilitate unit testing by extracting a
    startHTTPServers function and encapsulating HTTP server configuration
    in a ServerConfig struct.
    
    Also introduced a StringSliceFlag type to handle a comma-separated
    string and modified the "ignore-namespaces" flag to use it as well
    for consistency.
    
    Signed-off-by: Tom Pantelis <tompantelis@gmail.com>

--- cmd/webhook/main.go
+++ cmd/webhook/main.go
@@ -16,6 +16,7 @@
 package main
 
 import (
+	"context"
 	"crypto/sha512"
 	"crypto/tls"
 	"encoding/hex"
@@ -25,6 +26,7 @@ import (
 	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 	"syscall"
 	"time"
 
@@ -36,6 +38,7 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	utilwait "k8s.io/apimachinery/pkg/util/wait"
+	cliflag "k8s.io/component-base/cli/flag"
 )
 
 const (
@@ -43,15 +46,55 @@ const (
 	healthzPath = "/healthz"
 )
 
+// ServerConfig holds configuration for the HTTP servers
+type ServerConfig struct {
+	Port            int
+	Address         string
+	MetricsAddress  string
+	EncryptMetrics  bool
+	TLSMinVersion   string
+	TLSCipherSuites StringSliceFlag
+	GetCertificate  func(*tls.ClientHelloInfo) (*tls.Certificate, error)
+}
+
+// StringSliceFlag implements flag.Value interface for comma-separated string lists
+type StringSliceFlag []string
+
+func (s *StringSliceFlag) String() string {
+	if s == nil {
+		return ""
+	}
+	return strings.Join(*s, ",")
+}
+
+func (s *StringSliceFlag) Set(value string) error {
+	parts := strings.Split(value, ",")
+	*s = make([]string, 0, len(parts))
+	for _, part := range parts {
+		trimmed := strings.TrimSpace(part)
+		if trimmed != "" {
+			*s = append(*s, trimmed)
+		}
+	}
+	return nil
+}
+
 func main() {
 	// load configuration
-	port := flag.Int("port", 443, "The port on which to serve.")
-	address := flag.String("bind-address", "0.0.0.0", "The IP address on which to listen for the --port port.")
-	metricsAddress := flag.String("metrics-listen-address", ":9091", "metrics server listen address.")
-	encryptMetrics := flag.Bool("encrypt-metrics", false, "serve metrics over HTTPS using tls-cert-file/tls-private-key-file x509 key pair")
 	cert := flag.String("tls-cert-file", "cert.pem", "File containing the default x509 Certificate for HTTPS.")
 	key := flag.String("tls-private-key-file", "key.pem", "File containing the default x509 private key matching --tls-cert-file.")
-	ignoreNamespaces := flag.String("ignore-namespaces", "", "Comma separated namespace list to ignore pod update")
+
+	config := &ServerConfig{}
+	flag.IntVar(&config.Port, "port", 443, "The port on which to serve.")
+	flag.StringVar(&config.Address, "bind-address", "0.0.0.0", "The IP address on which to listen for the --port port.")
+	flag.StringVar(&config.MetricsAddress, "metrics-listen-address", ":9091", "metrics server listen address.")
+	flag.BoolVar(&config.EncryptMetrics, "encrypt-metrics", false, "serve metrics over HTTPS using tls-cert-file/tls-private-key-file x509 key pair")
+	flag.StringVar(&config.TLSMinVersion, "tls-min-version", "", "Minimum TLS version supported")
+	flag.Var(&config.TLSCipherSuites, "tls-cipher-suites", "Comma-separated list of cipher suites")
+
+	var ignoreNamespaces StringSliceFlag
+	flag.Var(&ignoreNamespaces, "ignore-namespaces", "Comma separated namespace list to ignore pod update")
+
 	flag.Parse()
 
 	glog.Infof("starting net-attach-def-admission-controller webhook server")
@@ -60,6 +103,7 @@ func main() {
 	if err != nil {
 		glog.Fatalf("error load certificate: %s", err.Error())
 	}
+	config.GetCertificate = keyPair.GetCertificateFunc()
 
 	proc, err := os.FindProcess(os.Getpid())
 	if err != nil {
@@ -76,43 +120,16 @@ func main() {
 
 	// init API client
 	webhook.SetupInClusterClient()
-	// start metrics sever
-	if *encryptMetrics {
-		startHTTPMetricServer(*metricsAddress, &tls.Config{
-			GetCertificate: keyPair.GetCertificateFunc(),
-		})
-	} else {
-		startHTTPMetricServer(*metricsAddress, nil)
+
+	// Start HTTP servers (metrics and webhook)
+	cleanup, err := startHTTPServers(config)
+	if err != nil {
+		glog.Fatalf("error starting HTTP servers: %v", err)
 	}
+	defer cleanup()
 
 	// Start watching for pod creations
 	go controller.StartWatching(ignoreNamespaces)
-
-	go func() {
-		// register handlers
-		var httpServer *http.Server
-		http.HandleFunc("/validate", webhook.ValidateHandler)
-		http.HandleFunc("/isolate", webhook.IsolateHandler)
-
-		// start serving
-		httpServer = &http.Server{
-			Addr: fmt.Sprintf("%s:%d", *address, *port),
-			TLSConfig: &tls.Config{
-				GetCertificate: keyPair.GetCertificateFunc(),
-				MinVersion: tls.VersionTLS12,
-				CipherSuites: []uint16{
-					tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-					tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-					tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
-				},
-			},
-		}
-
-		err := httpServer.ListenAndServeTLS("", "")
-		if err != nil {
-			glog.Fatalf("error starting web server: %v", err)
-		}
-	}()
 
 	// watch the cert file and restart http sever if the file updated.
 	oldHashVal := ""
@@ -142,7 +159,84 @@ func main() {
 
 }
 
-func startHTTPMetricServer(metricsAddress string, tlsConfig *tls.Config) {
+func startHTTPServers(config *ServerConfig) (func(), error) {
+	// Parse TLS configuration
+	tlsCipherSuiteIDs, err := cliflag.TLSCipherSuites(config.TLSCipherSuites)
+	if err != nil {
+		return nil, fmt.Errorf("error parsing TLS cipher suites %v: %w", config.TLSCipherSuites, err)
+	}
+
+	var tlsMinVersionID uint16
+	if config.TLSMinVersion != "" {
+		tlsMinVersionID, err = cliflag.TLSVersion(config.TLSMinVersion)
+		if err != nil {
+			return nil, fmt.Errorf("error parsing TLS min version %q: %w", config.TLSMinVersion, err)
+		}
+	}
+
+	applyTLSOptions := func(to *tls.Config) *tls.Config {
+		if tlsMinVersionID != 0 {
+			to.MinVersion = tlsMinVersionID
+		}
+
+		if len(tlsCipherSuiteIDs) > 0 {
+			to.CipherSuites = tlsCipherSuiteIDs
+		}
+
+		to.GetCertificate = config.GetCertificate
+
+		return to
+	}
+
+	// Start metrics server
+	var metricsServer *http.Server
+	if config.EncryptMetrics {
+		metricsServer = startHTTPMetricServer(config.MetricsAddress, applyTLSOptions(&tls.Config{
+			MinVersion: tls.VersionTLS12,
+		}))
+	} else {
+		metricsServer = startHTTPMetricServer(config.MetricsAddress, nil)
+	}
+
+	// Start webhook server
+	webhookServer := &http.Server{
+		Addr: fmt.Sprintf("%s:%d", config.Address, config.Port),
+		TLSConfig: applyTLSOptions(&tls.Config{
+			MinVersion: tls.VersionTLS12,
+			CipherSuites: []uint16{
+				tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+				tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+				tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256,
+			},
+		}),
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/validate", webhook.ValidateHandler)
+	mux.HandleFunc("/isolate", webhook.IsolateHandler)
+	webhookServer.Handler = mux
+
+	go func() {
+		err := webhookServer.ListenAndServeTLS("", "")
+		if err != nil && err != http.ErrServerClosed {
+			glog.Fatalf("error starting web server: %v", err)
+		}
+	}()
+
+	return func() {
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := webhookServer.Shutdown(ctx); err != nil {
+			glog.Errorf("error shutting down webhook server: %v", err)
+		}
+		if err := metricsServer.Shutdown(ctx); err != nil {
+			glog.Errorf("error shutting down metrics server: %v", err)
+		}
+	}, nil
+}
+
+func startHTTPMetricServer(metricsAddress string, tlsConfig *tls.Config) *http.Server {
 	mux := http.NewServeMux()
 	mux.Handle(metricsPath, promhttp.Handler())
 
@@ -165,24 +259,23 @@ func startHTTPMetricServer(metricsAddress string, tlsConfig *tls.Config) {
 		 </html>`))
 	})
 
-	go utilwait.Until(func() {
-		if tlsConfig != nil {
-			srv := &http.Server{
-				Addr:      metricsAddress,
-				TLSConfig: tlsConfig,
-				Handler:   mux,
-			}
+	srv := &http.Server{
+		Addr:      metricsAddress,
+		TLSConfig: tlsConfig,
+		Handler:   mux,
+	}
 
-			err := srv.ListenAndServeTLS("", "")
-			if err != nil {
-				glog.Fatalf("starting tls metrics server failed: %v", err)
-			}
+	go utilwait.Until(func() {
+		var err error
+		if tlsConfig != nil {
+			err = srv.ListenAndServeTLS("", "")
 		} else {
-			err := http.ListenAndServe(metricsAddress, mux)
-			if err != nil {
-				utilruntime.HandleError(fmt.Errorf("starting metrics server failed: %v", err))
-			}
+			err = srv.ListenAndServe()
+		}
+		if err != nil && err != http.ErrServerClosed {
+			utilruntime.HandleError(fmt.Errorf("metrics server failed: %v", err))
 		}
 	}, 5*time.Second, utilwait.NeverStop)
 
+	return srv
 }
